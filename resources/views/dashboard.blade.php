8ff81fad92e00a7e457783d51c3998bdc0131489
Thomas Chadenga <thomaschadenga@gmail.com>
2022-12-04 21:36:41 -0600

    add subscription buttons and subscriptions controller

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,6 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Dashboard') }}</div>
-
                     <div class="card-body">
                         @if (session('success'))
                             <div class="alert alert-success unique" role="alert">
@@ -14,7 +13,48 @@
                             </div>
                         @endif
 
-                        You have been logged in.
+                        These are your basic stats.
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">Extended Stats</div>
+                    <div class="card-body">
+                        These are your subscribed stats.
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">Subscribe To Our Monthly Subscription - $9.99</div>
+                    <div class="card-body">
+
+                        <form action="{{ route('subscribe.monthly') }}" method="POST">
+                            @csrf
+                            <input type="hidden" id="monthly" class="form-control" name="monthly" value="1" required>
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary green">
+                                    Monthly Subscription
+                                </button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">Subscribe To Our Yearly Subscription - $99.99</div>
+                    <div class="card-body">
+
+                        <form action="{{ route('subscribe.yearly') }}" method="POST">
+                            @csrf
+                            <input type="hidden" id="yearly" class="form-control" name="yearly" value="1" required>
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary green">
+                                    Yearly Subscription
+                                </button>
+                            </div>
+                        </form>
+
                     </div>
                 </div>
             </div>
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscriptionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->post('subscribe/monthly', [SubscriptionController::class, 'monthly'])->name('subscribe.monthly');
+Route::middleware('auth:sanctum')->post('subscribe/yearly', [SubscriptionController::class, 'yearly'])->name('subscribe.yearly');
